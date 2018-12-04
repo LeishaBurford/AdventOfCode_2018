01ab9f54c81b37aacd4206beb6151910f95c9b0b
Leisha Burford <lburford@brocksolutions.com>
2018-12-04 17:03:08 -0500

    converted input to integers

--- Day_1/Day_1.py
+++ Day_1/Day_1.py
@@ -9,16 +9,35 @@ with open('C:\git\AdventOfCode_2018\Day_1\input.txt') as f:
     frequencies = f.read().splitlines()
 
 
+# In[4]:
+
+
+def IsPositive(char):
+    return char == '+'
+
+
 # In[ ]:
 
 
 
 
 
+# In[10]:
+
+
+frequenciesAsIntegers = [int(value[1:]) if IsPositive(value[0]) else int(value[1:]) * -1 for value in frequencies]
+
+
 # In[ ]:
 
 
 
+
+
+# In[11]:
+
+
+print(frequenciesAsIntegers)
 
 
 # In[ ]:
