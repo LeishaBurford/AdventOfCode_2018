908b910c3dbb3afbf3a08b29d7d8d76698c88675
Leisha Burford <lburford@brocksolutions.com>
2018-12-04 16:50:11 -0500

    add .py script, read input data

---
+++ Day_1/Day_1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+with open('C:\git\AdventOfCode_2018\Day_1\input.txt') as f:
+    frequencies = f.read().splitlines()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
