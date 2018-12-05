929fb7b4bdc1746d440d95a23d967349252cee96
Leisha Burford <lburford@brocksolutions.com>
2018-12-05 11:09:16 -0500

    completed day one

--- Day_1/Day_1.py
+++ Day_1/Day_1.py
@@ -1,43 +1,66 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
-
+# part 1
 with open('C:\git\AdventOfCode_2018\Day_1\input.txt') as f:
-    frequencies = f.read().splitlines()
+    inputData = f.read().splitlines()
 
 
-# In[4]:
+# In[3]:
 
 
 def IsPositive(char):
     return char == '+'
 
 
+# In[7]:
+
+
+frequencies = [int(value[1:]) if IsPositive(value[0]) else int(value[1:]) * -1 for value in inputData]
+print(sum(frequencies))
+
+
+# In[ ]:
+
+
+# part 2
+
+
+# In[20]:
+
+
+encounteredFrequencies = {}
+currentFrequency = 0
+foundDuplicate = False
+while not foundDuplicate:
+    for value in frequencies:
+        currentFrequency += value
+        if (currentFrequency in encounteredFrequencies):
+            print(currentFrequency)
+            foundDuplicate = True
+            break
+        encounteredFrequencies[currentFrequency] = 1
+
+
 # In[ ]:
 
 
 
 
 
-# In[10]:
-
-
-frequenciesAsIntegers = [int(value[1:]) if IsPositive(value[0]) else int(value[1:]) * -1 for value in frequencies]
-
-
 # In[ ]:
 
 
 
 
 
-# In[11]:
+# In[ ]:
 
 
-print(frequenciesAsIntegers)
+
 
 
 # In[ ]:
