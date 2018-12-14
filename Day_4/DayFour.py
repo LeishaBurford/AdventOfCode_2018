ab96b19cf9daf6f1e0d112714e6fb7e8e3f0329a
LeishaBurford <leishabobisha@gmail.com>
2018-12-13 19:16:14 -0500

    finsihed day 4

--- Day_4/DayFour.py
+++ Day_4/DayFour.py
@@ -1,13 +1,76 @@
-#!/usr/bin/env python
-
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 # part 1
-with open('c:\git\AdventOfCode_2018\Day_4\input.txt') as f:
+with open('input.txt') as f:
     guardRecords = f.read().splitlines()
 
-# sort set of data by
-guardRecords = [a for a in guardRecords if a != 'ÿþ']
-before = len(guardRecords)
-guardRecords.sort(key=lambda x: x.split(' ')[0])
+# sort the data by datetime
+guardRecords.sort(key=lambda x: ''.join(x.split(' ')[0:2]))
 
+def newGuard(record):
+    return '#' in record
+
+def getNewGuardId(record):
+    return int(record.split(' ')[3][1:])
+
+def guardFellAsleep(record):
+    return 'asleep' in record
+
+def currentMinute(record):
+    return int(''.join(record.split(' ')[1][3:5]))
+
+def guardWokeUp(record):
+    return 'wakes' in record
+
+def updateSleepTime(oldSleepTimes, startMinute, endMinute):
+    for time in range(startMinute, endMinute):
+        oldSleepTimes[time] += 1
+    return oldSleepTimes
+
+sleeptimes = {}
+currentGuard = 0
+sleepStartTime = 0
 for record in guardRecords:
-    print(record)
+    # get the id for the guard this corresponds to 
+    # find the time they are asleep
+    # find the time they are awake
+    if (newGuard(record)):
+        currentGuard = getNewGuardId(record)
+        if currentGuard not in sleeptimes:
+            sleeptimes[currentGuard] = [0 for i in range(60)]
+    elif (guardFellAsleep(record)): 
+        sleepStartTime = currentMinute(record)
+    elif (guardWokeUp(record)):  
+        sleeptimes[currentGuard] = updateSleepTime(sleeptimes[currentGuard], sleepStartTime, currentMinute(record))
+
+# for sleepRecord in sleeptimes:
+#    print(sleepRecord)
+#    print(sleeptimes[sleepRecord])
+
+# find sleepiest guard -- I'm sure there's a better way to do this
+recordSleepTime = 0
+sleepiestGuard = 0
+for guard in sleeptimes:
+    sleepMinutes = sum(sleeptimes[guard])
+    if sleepMinutes > recordSleepTime:
+        recordSleepTime = sleepMinutes
+        sleepiestGuard = guard
+
+sleepiestMinute = sleeptimes[sleepiestGuard].index(max(sleeptimes[sleepiestGuard]))
+print("guard", sleepiestGuard)
+print("minute", sleepiestMinute)
+print(sleepiestGuard * sleepiestMinute)
+
+# part 2
+recordSleepiestMinute = 0
+sleepiestGuard = 0
+for guard in sleeptimes:
+    sleepiestMinute = max(sleeptimes[guard])
+    if sleepiestMinute > recordSleepiestMinute:
+        recordSleepiestMinute = sleepiestMinute
+        sleepiestGuard = guard
+    
+sleepiestMinute = sleeptimes[sleepiestGuard].index(recordSleepiestMinute)
+print("guard", sleepiestGuard)
+print("minute", sleepiestMinute)
+print(sleepiestGuard * sleepiestMinute)
