a397183d4eabb17743a3ab2401f98e25c35df882
Leisha Burford <lburford@brocksolutions.com>
2018-12-17 13:02:56 -0500

    part 1 done

--- Day_6/DaySix.py
+++ Day_6/DaySix.py
@@ -1,12 +1,15 @@
-with open('testinput.txt') as f:
+with open('C:/git/AdventOfCode_2018/Day_6/input.txt') as f:
     coordinates = f.read().splitlines()
 
 
 def computeDistance(a, b, c, d):
     return abs(a - c) + abs(b - d)
+
 coordinates = [coordinate.split(',') for coordinate in coordinates]
+
 coordinates = list(
     map(lambda x: (list(map(lambda y: int(y) - 1, x))), coordinates))
+
 # x for xy in coor
 xRange = [min([x for x, y in coordinates]), max([x for x, y in coordinates])]
 yRange = [min([y for x, y in coordinates]), max([y for x, y in coordinates])]
@@ -17,22 +20,12 @@ grid = [[-1 for i in range(0, xRange[1] + 2)]
 print(xRange, yRange)
 print(len(grid[0]), len(grid))
 
-# for row in grid:
-# 	print(row)
-
-# add the coordinates
 val = 0
 for x, y in coordinates:
     grid[y][x] = val
     val += 1
-# for x, y in coordinates:
-    # print(x, y)
- #    grid[y][x] = val
- #    val += 1
 names = [i for i in range(val)]
 coordinateIndex = dict(zip(names, coordinates))
-# print(closestPoints)
-
 closestPoints = dict.fromkeys(names, 0)
 
 infinitePoints = []
@@ -41,7 +34,7 @@ infinitePoints = []
 def determineClosestPoint(x, y, points):
     distances = [computeDistance(x, y, c[0], c[1]) for c in points]
     # if equally close to 2+ points, return -1
-    return distances.index(min(distances)) if len(distances) == len(set(distances)) else -1
+    return distances.index(min(distances)) if distances.count(min(distances)) == 1 else -1
 
 
 def isOnEdge(x, y):
@@ -52,19 +45,14 @@ for y in range(len(grid)):
         if grid[y][x] == -1:
             closestPoint = determineClosestPoint(x, y, coordinates)
             if closestPoint != -1:
-            	grid[y][x] = closestPoint
+                grid[y][x] = closestPoint
                 closestPoints[closestPoint] += 1
             else:
-            	grid[y][x] = 8
+                grid[y][x] = 8
             if isOnEdge(x, y):
                 infinitePoints.append(closestPoint)
         else:
             closestPoints[grid[y][x]] += 1
-
-for row in grid:
-	print(row)
-
-print(closestPoints.values())
 
 print(max([closestPoints[closestPoint]
            for closestPoint in closestPoints if closestPoint not in infinitePoints]))
