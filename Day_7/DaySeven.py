ba5be1bf98859eadf1fea62a3041651f7086cb1a
Leisha Burford <lburford@brocksolutions.com>
2018-12-18 15:18:38 -0500

    fixed odd behaviour

--- Day_7/DaySeven.py
+++ Day_7/DaySeven.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[49]:
+# In[138]:
 
 
 with open('C:\git\AdventOfCode_2018\Day_7\input.txt') as f:
     instructions = f.read().splitlines()
 
 
-# In[50]:
+# In[139]:
 
 
 # part 1
 instructions = [(instruction[1], instruction[7]) for instruction in list(map(lambda x: x.split(' '), instructions))]
 
 
-# In[51]:
+# In[140]:
 
 
-instructionRequirements = dict.fromkeys(list(set([y for x, y in instructions])), [])
+instructionRequirements = { instruction : [] for instruction in list(set([y for x, y in instructions])) }
 
 
-# In[52]:
+# In[141]:
 
 
 # for each requirement, add to dictionary
@@ -29,9 +29,9 @@ instructionRequirements = dict.fromkeys(list(set([y for x, y in instructions])),
 # find an instruction that is not in the requirements -- maybe maintain this in another dictionary
 
 # setup
-instructionHasRequirement = dict.fromkeys(list(set([y for x, y in instructions])), False)
+instructionHasRequirement = dict.fromkeys(list(set([x for x, y in instructions])), False)
+
 for requirement, instruction in instructions:
-    print(requirement, instruction)
     instructionRequirements[instruction].append(requirement)
     instructionHasRequirement[instruction] = True
 instructionRequirements
