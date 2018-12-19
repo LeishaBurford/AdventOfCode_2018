fc3ba2676a59f19572d97ab71b2cfef7c53ddf09
Leisha Burford <lburford@brocksolutions.com>
2018-12-19 16:47:25 -0500

    part one done

--- Day_7/DaySeven.py
+++ Day_7/DaySeven.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 
-with open('testinput.txt') as f:
+with open('C:\git\AdventOfCode_2018\Day_7\input.txt') as f:
     instructions = f.read().splitlines()
 
 # part 1
+
 instructions = [(instruction[1], instruction[7])
                 for instruction in list(map(lambda x: x.split(' '), instructions))]
 instructionRequirements = {instruction: []
                            for instruction in list(set([y for x, y in instructions]))}
-
 # setup
-instructionHasRequirement = dict.fromkeys(
-    list(set([x for x, y in instructions])), False)
+instructionHasRequirement = {x : False for x in list(set([x for x, y in instructions]))}
 
 for requirement, instruction in instructions:
     instructionRequirements[instruction].append(requirement)
     instructionHasRequirement[instruction] = True
 
-ready = sorted([instruction for instruction in instructionHasRequirement.keys(
-) if not instructionHasRequirement[instruction]], reverse=True)
+ready = sorted([instruction for instruction in instructionHasRequirement.keys() if not instructionHasRequirement[instruction]], reverse=True)
+for instruction in ready:
+    if not instructionHasRequirement:
+        instructionHasRequirement.pop(newInstruction)
 
 result = ''
 while(ready):
@@ -30,9 +31,7 @@ while(ready):
         if newInstruction in instructionRequirements[instruction]:
             instructionRequirements[instruction].remove(newInstruction)
             # if req's list is now empty, update the bool
-            instructionHasRequirement[
-                instruction] = False if not instructionHasRequirement[instruction] else True
-    ready += sorted([instruction for instruction in instructionHasRequirement.keys(
-    ) if not instructionHasRequirement[instruction]], reverse=True)
-
+            instructionHasRequirement[instruction] = True if instructionRequirements[instruction] else False
+    
+    ready += sorted([instruction for instruction in instructionHasRequirement if not instructionHasRequirement[instruction] and instruction not in ready], reverse=True)
 print(result)
