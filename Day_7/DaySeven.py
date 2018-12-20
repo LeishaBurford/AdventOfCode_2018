6ba8029996b535de691be7b6f77f1a7ee3a6e127
Leisha Burford <lburford@brocksolutions.com>
2018-12-20 12:30:31 -0500

    update

--- Day_7/DaySeven.py
+++ Day_7/DaySeven.py
@@ -54,30 +54,38 @@ for instruction in ready:
 result = ''
 workers = {x : [-1, ''] for x in range(2)}
 timePassed = 0
-while(ready):
+
+while(ready or [x for x in workers if workers[x][0] >= 0] ):
     # assign instruction to worker
     print('current workers', workers)
-    if -1 in workers.values():
+    availableWorkers = [x for x in workers if workers[x][0] < 0]
+    while(availableWorkers and ready):
         newInstruction = ready.pop()
         instructionHasRequirement.pop(newInstruction)
-        worker = [y[0] for x, y in workers if y[0] == -1][0]
+        # get the available worker
+        worker = availableWorkers[0]
         # set the time
-        workers[worker] = [ord(newInstruction) - 64, newInstruction]
+        workers[worker] = [ord(newInstruction) - ord('A') + 1, newInstruction]
+        availableWorkers = [x for x in workers if workers[x][0] < 0]
         print('new isntruction', workers)
     # if worker is done
     doneInstrcutions = sorted([workers[x][1] for x in workers if workers[x][0] == 0], reverse=True)
     print('doneInstructions', doneInstrcutions)
     if doneInstrcutions:
-        result += doneInstrcutions
         # update instruction requirements
-        for instruction in instructionRequirements:
-            if newInstruction in instructionRequirements[instruction]:
-                instructionRequirements[instruction].remove(newInstruction)
-                # if req's list is now empty, update the bool
-                instructionHasRequirement[instruction] = True if instructionRequirements[instruction] else False
+        for doneInstruction in doneInstrcutions:
+            result += doneInstruction
+            for instruction in instructionRequirements:
+                if doneInstruction in instructionRequirements[instruction]:
+                    instructionRequirements[instruction].remove(doneInstruction)
+                    # if req's list is now empty, update the bool
+                    instructionHasRequirement[instruction] = True if instructionRequirements[instruction] else False
         ready += sorted([instruction for instruction in instructionHasRequirement if not instructionHasRequirement[instruction] and instruction not in ready], reverse=True)
+    # increase time
+    # decrease time remaining for workeers
     for worker in workers:
         workers[worker][0] -= 1
         print('updated workesr', workers)
     timePassed += 1
 print(timePassed)
+print(result)
